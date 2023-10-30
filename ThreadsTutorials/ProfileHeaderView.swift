5affc19da23aed290322f32a5dce19777137f951
Byungho <57972951+ho-1@users.noreply.github.com>
2023-10-31 00:05:00 +0900

    feat: 본인 프로필, 유저 프로필 분리
    * 일부 코드 리펙토링

---
+++ ThreadsTutorials/ProfileHeaderView.swift
@@ -0,0 +1,49 @@
+//
+//  ProfileHeaderView.swift
+//  ThreadsTutorials
+//
+//  Created by byungho on 10/30/23.
+//
+
+import SwiftUI
+
+struct ProfileHeaderView: View {
+    var user: User?
+    
+    init(user: User?) {
+        self.user = user
+    }
+    
+    var body: some View {
+        HStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 12) {
+                // fullname and username
+                VStack(alignment: .leading, spacing: 4) {
+                    Text(user?.fullname ?? "")
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                    
+                    Text(user?.username ?? "")
+                        .font(.subheadline)
+                }
+                
+                if let bio = user?.bio {
+                    Text(bio)
+                        .font(.footnote)
+                }
+                
+                Text("2 followers")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            
+            Spacer()
+            
+            CircularProfileImageView()
+        }
+    }
+}
+
+#Preview {
+    ProfileHeaderView(user: DeveloperPreview.shared.user)
+}
--- ThreadsTutorials/ProfileView.swift
+++ ThreadsTutorials/ProfileView.swift
@@ -8,119 +8,34 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @StateObject var viewModel = ProfileViewModel()
-    @State private var selectedFilter: ProfileThreadFilter = .threads
-    @Namespace var animation
-    
-    private var filterBarWidth: CGFloat {
-        let count = CGFloat(ProfileThreadFilter.allCases.count)
-        return UIScreen.main.bounds.width / count - 16
-    }
-    
-    private var currentUser: User? {
-        return viewModel.currentUser
-    }
-    
+    let user: User
+
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                // bio and stats
-                VStack(spacing: 20) {
-                    HStack(alignment: .top) {
-                        VStack(alignment: .leading, spacing: 12) {
-                            // fullname and username
-                            VStack(alignment: .leading, spacing: 4) {
-                                Text(currentUser?.fullname ?? "")
-                                    .font(.title2)
-                                    .fontWeight(.semibold)
-                                
-                                Text(currentUser?.username ?? "")
-                                    .font(.subheadline)
-                            }
-                            
-                            if let bio = currentUser?.bio {
-                                Text(bio)
-                                    .font(.footnote)
-                            } else {
-                                Text("한 줄 소개")
-                                    .font(.footnote)
-                                    .foregroundColor(Color(.systemGray4))
-                            }
-                            
-                            Text("2 followers")
-                                .font(.caption)
-                                .foregroundColor(.gray)
-                        }
-                        
-                        Spacer()
-                        
-                        CircularProfileImageView()
-                    }
+        ScrollView(showsIndicators: false) {
+            // bio and stats
+            VStack(spacing: 20) {
+                ProfileHeaderView(user: user)
+                
+                Button {
                     
-                    Button {
-                        
-                    } label: {
-                        Text("Follow")
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .foregroundColor(.white)
-                            .frame(width: 352, height: 32)
-                            .background(.black)
-                            .cornerRadius(8)
-                    }
-                    
-                    // user content list view
-                    VStack {
-                        HStack {
-                            ForEach(ProfileThreadFilter.allCases) { filter in
-                                VStack {
-                                    Text(filter.title)
-                                        .font(.subheadline)
-                                        .fontWeight(selectedFilter == filter ? .semibold : .regular)
-                                    
-                                    if selectedFilter == filter {
-                                        Rectangle()
-                                            .foregroundColor(.black)
-                                            .frame(width: filterBarWidth, height: 1)
-                                            .matchedGeometryEffect(id: "item", in: animation)
-                                        
-                                    } else {
-                                        Rectangle()
-                                            .foregroundColor(.clear)
-                                            .frame(width: filterBarWidth, height: 1)
-                                    }
-                                }
-                                .onTapGesture {
-                                    withAnimation(.spring()) {
-                                        selectedFilter = filter
-                                    }
-                                }
-                            }
-                        }
-                        
-                        LazyVStack {
-                            ForEach(0 ... 10, id: \.self) { thread in
-                                ThreadCell()
-                            }
-                        }
-                    }
-                    .padding(.vertical, 8)
+                } label: {
+                    Text("Follow")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 352, height: 32)
+                        .background(.black)
+                        .cornerRadius(8)
                 }
+                
+                UserContentListView()
             }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button {
-                        AuthService.shared.signout()
-                    } label: {
-                        Image(systemName: "line.3.horizontal")
-                    }
-                }
-            }
-            .padding(.horizontal)
         }
+        .navigationBarTitleDisplayMode(.inline)
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    ProfileView()
+    ProfileView(user: DeveloperPreview.shared.user)
 }
